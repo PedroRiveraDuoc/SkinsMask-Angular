eac6d98b1905f41b941dc3c5eb6e6d9d31de5ba5
PedroRiveraDuoc <p.riveral@duocuc.cl>
2024-07-06 02:43:10 -0400

    Inicio de Upload images

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
  * Este componente maneja la visualización de los productos y permite agregar
  * productos al carrito de compras.
  */
-
+// Interfaz que define la estructura de un producto
 interface Product {
   id: number;
   name: string;
@@ -29,7 +29,9 @@ interface Product {
   providers: [ProductsService]  // Asegurarse de que el servicio esté disponible
 })
 export class ProductsComponent implements OnInit {
+  // Arreglo para almacenar los productos
   products: Product[] = [];
+  // Objeto para manejar el formulario de productos
   productForm: any = {
     id: null,
     name: '',
@@ -37,17 +39,21 @@ export class ProductsComponent implements OnInit {
     price: null,
     imageUrl: ''
   };
+  // Bandera para determinar si el modo de edición está activo
   isEditMode: boolean = false;
 
+  // Constructor que inyecta los servicios de carrito y productos
   constructor(private cartService: CartService, private productsService: ProductsService) {
     console.log('ProductsComponent constructor called');
   }
 
+  // Método de inicialización que carga los productos al iniciar el componente
   ngOnInit(): void {
     console.log('ProductsComponent ngOnInit called');
     this.loadProducts();
   }
 
+  // Método para cargar los productos desde el archivo JSON
   loadProducts(): void {
     console.log('loadProducts method called');
     this.productsService.getProducts().subscribe(
@@ -61,11 +67,13 @@ export class ProductsComponent implements OnInit {
     );
   }
 
+  // Método para cargar los datos de un producto en el formulario y activar el modo de edición
   modificar(producto: Product): void {
     this.productForm = { ...producto };
     this.isEditMode = true;
   }
 
+  // Método para eliminar un producto de la lista y actualizar el archivo JSON
   eliminar(producto: Product): void {
     const index = this.products.findIndex((p: Product) => p.id === producto.id);
     if (index !== -1) {
@@ -76,14 +84,19 @@ export class ProductsComponent implements OnInit {
     }
   }
 
+
+  // Método para agregar un producto al carrito
   addProductToCart(product: Product): void {
     this.cartService.addToCart(product);
     alert('Producto agregado al carrito');
   }
 
+
+  // Método para enviar el formulario, agrega o actualiza un producto en la lista y en el archivo JSON
   submitForm(): void {
     console.log('Form submitted');
     if (this.isEditMode) {
+      // Si está en modo de edición, actualiza el producto existente
       const index = this.products.findIndex((p: Product) => p.id === this.productForm.id);
       if (index !== -1) {
         this.products[index] = { ...this.productForm };
@@ -91,6 +104,7 @@ export class ProductsComponent implements OnInit {
         this.isEditMode = false;
       }
     } else {
+      // Si no está en modo de edición, agrega un nuevo producto
       const newProduct: Product = {
         id: this.products.length > 0 ? Math.max(...this.products.map(p => p.id)) + 1 : 1,
         name: this.productForm.name,
@@ -101,6 +115,7 @@ export class ProductsComponent implements OnInit {
       this.products.push(newProduct);
       this.productsService.MetodoProductos(this.products);
     }
+    // Resetea el formulario
     this.productForm = {
       id: null,
       name: '',
@@ -109,4 +124,15 @@ export class ProductsComponent implements OnInit {
       imageUrl: ''
     };
   }
+
+  file!: File;
+  changeInput(event: Event): void{
+    const input = event.target as HTMLInputElement;
+    if (input.files) {
+      this.file = input.files[0];
+    }
+    
+  }
+
+
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { response } from 'express';
 
 /**
  * ProductsService
@@ -13,28 +12,43 @@ import { response } from 'express';
 })
 export class ProductsService {
 
+  // Opciones HTTP que incluyen cabeceras necesarias para la autorización y tipo de contenido
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Bearer 39ad5402-8865-43a9-b5ac-f151db8a3394'
     })
   };
-
+  // URL del archivo JSON en Firebase
   private jsonUrl = 'https://firebasestorage.googleapis.com/v0/b/skinsmask-baa17.appspot.com/o/products.json?alt=media&token=39ad5402-8865-43a9-b5ac-f151db8a3394'; // URL archivo JSON Firebase
 
-
+  // Variable para almacenar la lista de productos
   private lista: any;
-
+  
+  // Constructor que inyecta HttpClient y muestra un mensaje en la consola cuando se instancia el servicio
   constructor(private http: HttpClient) {
     console.log('ProductsService constructor called');
   }
 
 
+
+  /**
+   * Método para obtener los productos desde el archivo JSON.
+   * 
+   * @returns Observable que emite la lista de productos.
+   */
   getProducts(): Observable<any> {
     console.log('getProducts method called');
     return this.http.get(this.jsonUrl);
   }
 
+
+  
+  /**
+   * Método para sobrescribir el archivo JSON con una nueva lista de productos.
+   * 
+   * @param listaProductos - La nueva lista de productos que se va a guardar en el archivo JSON.
+   */
   MetodoProductos(listaProductos: any) {
     console.log(listaProductos);
     this.http.post(this.jsonUrl, listaProductos, this.httpOptions).subscribe(
